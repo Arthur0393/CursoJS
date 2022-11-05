35c5d4e6cdb8d75a876353bf4b5231e3cbcca456
Arthur0393 <carlos.ara777@gmail.com>
2022-11-04 19:54:36 -0600

    Seccion 2

---
+++ assets/js/alerts.js
@@ -0,0 +1,15 @@
+
+// alert('Hola Mundo');
+
+// let nombre = prompt('¿Como te llamas', 'Sin nombre');
+
+// //Valor nulo
+// console.log( nombre ); 
+
+// //Valor String
+// console.log( '****' + nombre + '****' ); // ''
+
+    // const seleccion = confirm(' ¿Estas seguro de borrar esto? ');
+    // console.log( seleccion );
+
+    // console.log( global );
